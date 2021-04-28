d447e355f827c005d001c490c47b79588773921b
afridi11277 <shamsafridieece@gmail.com>
2021-04-28 17:27:45 +0600

    first commit

---
+++ js/ok.js
@@ -0,0 +1,15 @@
+//bx slider
+  $(document).ready(function(){
+    $('.slider_area').bxSlider();
+
+    $("#main_menu").meanmenu({
+          meanMenucontainer: "#mobilemenu",
+          meanScreenWidth: 1024,
+    })
+});
+
+/*
+
+jQuery(document).ready(function () {
+	jQuery('header nav').meanmenu();
+});*/
